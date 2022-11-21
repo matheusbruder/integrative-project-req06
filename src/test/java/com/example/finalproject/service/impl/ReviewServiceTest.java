3b1ba81c08b34e33d2d62b947cd86f8b9b209027
matheusbruder <matheus.bruder@mercadolivre.com>
2022-11-21 14:19:28 -0300

    :test_tube: refactor: unit tests

--- src/test/java/com/example/finalproject/service/impl/ReviewServiceTest.java
+++ src/test/java/com/example/finalproject/service/impl/ReviewServiceTest.java
@@ -4,7 +4,6 @@ import com.example.finalproject.exception.InvalidArgumentException;
 import com.example.finalproject.exception.NotFoundException;
 import com.example.finalproject.model.*;
 import com.example.finalproject.model.Enum.Category;
-import com.example.finalproject.model.Enum.OrderStatus;
 import com.example.finalproject.repository.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,7 +14,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,39 +39,24 @@ class ReviewServiceTest {
     @Mock
     SectionRepo sectionRepo;
 
-    private PurchaseOrder purchaseOrder;
     private PurchaseItem purchaseItem;
     private Advertisement advertisement;
     private List<Advertisement> advertisementList;
-    private List<PurchaseItem> purchaseItemList;
     private Buyer buyer;
-    private Batch batch;
-    private Batch batchII;
-    private InboundOrder inboundOrder;
     private Section section;
-    private Warehouse warehouse;
-    private List<Batch> batchList;
     private Review review;
     private List<Review> reviewList;
 
     @BeforeEach
     void setUp() {
-
         buyer = Buyer.builder()
                 .buyerCode(1L)
                 .name("Neymar")
                 .purchases(new ArrayList<>())
                 .build();
 
-        warehouse = Warehouse.builder()
-                .warehouseCode(1L)
-                .sections(new ArrayList<>())
-                .volume(10.0F)
-                .build();
-
         section = Section.builder()
                 .sectionCode(1L)
-                .warehouse(warehouse)
                 .category(Category.CONGELADO)
                 .volume(100.0F)
                 .accumulatedVolume(15.0F)
@@ -81,69 +64,20 @@ class ReviewServiceTest {
                 .maxTemperature(-18F)
                 .build();
 
-        batchList = new ArrayList<>();
-
-        inboundOrder = InboundOrder.builder()
-                .orderCode(3L)
-                .orderDate(LocalDate.of(2022, 11, 14))
-                .section(section)
-                .batchStock(batchList)
-                .build();
-
-        batch = Batch.builder()
-                .batchCode(3L)
-                .inboundOrder(inboundOrder)
-                .advertisement(advertisement)
-                .currentTemperature(-20.0F)
-                .productQuantity(100)
-                .manufacturingDateTime(LocalDateTime.of(2019, 1, 20, 22, 34))
-                .volume(12.0F)
-                .dueDate(LocalDate.of(2023, 1, 5))
-                .price(BigDecimal.valueOf(45.0D))
-                .build();
-
-        batchII = Batch.builder()
-                .batchCode(3L)
-                .inboundOrder(inboundOrder)
-                .advertisement(advertisement)
-                .currentTemperature(-20.0F)
-                .productQuantity(20)
-                .manufacturingDateTime(LocalDateTime.of(2019, 1, 20, 22, 34))
-                .volume(12.0F)
-                .dueDate(LocalDate.of(2023, 1, 5))
-                .price(BigDecimal.valueOf(45.0D))
-                .build();
-
-        batchList.add(batch);
-        batchList.add(batchII);
-
         advertisement = Advertisement.builder()
                 .advertisementCode(2L)
                 .name("Pizza")
                 .price(BigDecimal.valueOf(4.0))
-                .batches(batchList)
                 .seller(Seller.builder().build())
                 .build();
+        advertisementList = new ArrayList<>();
+        advertisementList.add(advertisement);
 
         purchaseItem = PurchaseItem.builder()
                 .itemCode(1L)
                 .price(BigDecimal.valueOf(22.0))
                 .quantity(100)
-                .purchaseOrder(purchaseOrder)
                 .advertisement(advertisement)
-                .build();
-
-        advertisementList = new ArrayList<>();
-        advertisementList.add(advertisement);
-        purchaseItemList = new ArrayList<>();
-        purchaseItemList.add(purchaseItem);
-
-        purchaseOrder = PurchaseOrder.builder()
-                .purchaseCode(1L)
-                .dateTime(LocalDateTime.of(2019, 1, 20, 22, 34))
-                .orderStatus(OrderStatus.ABERTO)
-                .buyer(buyer)
-                .purchaseItems(purchaseItemList)
                 .build();
 
         review = Review.builder()
@@ -160,112 +94,124 @@ class ReviewServiceTest {
 
     @Test
     void createReview_saveAndReturnReview_whenSuccess() {
+        // Arrange
         when(buyerRepo.findById(anyLong())).thenReturn(Optional.ofNullable(buyer));
         when(advertisementRepo.findById(anyLong())).thenReturn(Optional.ofNullable(advertisement));
         when(purchaseItemRepo.findByBuyerCodeAndAdvertisementCodeWhenPurchaseOrderFinished(buyer.getBuyerCode(), advertisement.getAdvertisementCode())).thenReturn(Optional.ofNullable(purchaseItem));
-
+        // Act
         reviewService.createReview(review);
-
+        // Assert
         verify(reviewRepo, times(1)).save(review);
     }
 
     @Test
     void createReview_returnInvalidArgumentException_whenBuyerHasNotBought() {
+        // Arrange
         when(buyerRepo.findById(anyLong())).thenReturn(Optional.ofNullable(buyer));
         when(advertisementRepo.findById(anyLong())).thenReturn(Optional.ofNullable(advertisement));
-
+        // Act ~ Assert
         assertThrows(InvalidArgumentException.class, () -> reviewService.createReview(review));
     }
 
     @Test
     void createReview_returnInvalidArgumentException_whenRepeatedReview() {
+        // Arrange
         when(buyerRepo.findById(anyLong())).thenReturn(Optional.ofNullable(buyer));
         when(advertisementRepo.findById(anyLong())).thenReturn(Optional.ofNullable(advertisement));
         when(reviewRepo.findAll()).thenReturn(reviewList);
-
+        // Act ~ Assert
         assertThrows(InvalidArgumentException.class, () -> reviewService.createReview(review));
     }
 
     @Test
     void updateReview_returnReview_whenSuccess() {
+        // Arrange
         when(reviewRepo.findById(anyLong())).thenReturn(Optional.ofNullable(review));
-
+        // Act
         reviewService.updateReview(review, review.getReviewCode());
-
+        // Assert
         verify(reviewRepo, times(1)).save(review);
     }
 
     @Test
     void updateReview_returnNotFoundException_whenReviewDoesNotExist() {
+        // Arrange
         when(reviewRepo.findById(anyLong())).thenReturn(Optional.empty());
-
+        // Act ~ Assert
         assertThrows(NotFoundException.class, () -> reviewService.updateReview(review, review.getReviewCode()));
     }
 
     @Test
     void findTopRatedAdvertisementsByCategory_returnAdvertisementList_whenSuccess() {
+        // Arrange
         when(advertisementRepo.findAllByCategory(anyString())).thenReturn(advertisementList);
         when(sectionRepo.findByCategory(anyString())).thenReturn(Optional.ofNullable(section));
-
+        // Act
         List<Advertisement> advertisementListFiltered = reviewService.findTopRatedAdvertisementsByCategory("congelado", 1);
-
+        // Assert
         Assertions.assertNotNull(advertisementListFiltered);
     }
 
     @Test
     void findTopRatedAdvertisementsByCategory_returnNotFoundException_whenCategoryDoesNotExist() {
+        // Arrange
         when(sectionRepo.findByCategory(anyString())).thenReturn(Optional.empty());
-
+        // Act ~ Assert
         assertThrows(NotFoundException.class, () -> reviewService.findTopRatedAdvertisementsByCategory("congelado", 1));
     }
 
     @Test
     void findTopRatedAdvertisementsByCategory_returnNotFoundException_whenAdvertisementListIsEmpty() {
+        // Arrange
         when(advertisementRepo.findAllByCategory(anyString())).thenReturn(new ArrayList<>());
         when(sectionRepo.findByCategory(anyString())).thenReturn(Optional.ofNullable(section));
-
-
+        // Act ~ Assert
         assertThrows(NotFoundException.class, () -> reviewService.findTopRatedAdvertisementsByCategory("congelado", 1));
     }
 
     @Test
     void findAllReviewsByBuyer_returnReviewListByBuyer_whenSuccess() {
+        // Arrange
         when(buyerRepo.findById(anyLong())).thenReturn(Optional.ofNullable(buyer));
         when(reviewRepo.findByBuyer(any())).thenReturn(reviewList);
-
+        // Act
         List<Review> reviewList = reviewService.findAllReviewsByBuyer(buyer.getBuyerCode());
-
+        // Assert
         Assertions.assertNotNull(reviewList);
     }
 
     @Test
     void findAllReviewsByBuyer_returnNotFoundException_whenBuyerDoesNotExist() {
+        // Arrange
         when(buyerRepo.findById(anyLong())).thenReturn(Optional.empty());
-
+        // Act ~ Assert
         assertThrows(NotFoundException.class, () -> reviewService.findAllReviewsByBuyer(buyer.getBuyerCode()));
     }
 
     @Test
     void findAllReviewsByBuyer_returnNotFoundException_whenBuyerHasNoReviews() {
+        // Arrange
         when(buyerRepo.findById(anyLong())).thenReturn(Optional.ofNullable(buyer));
         when(reviewRepo.findByBuyer(any())).thenReturn(new ArrayList<>());
-
+        // Act ~ Assert
         assertThrows(NotFoundException.class, () -> reviewService.findAllReviewsByBuyer(buyer.getBuyerCode()));
     }
 
     @Test
     void deleteReview_whenSuccess() {
+        // Arrange
         when(reviewRepo.findById(anyLong())).thenReturn(Optional.ofNullable(review));
-
+        // Act
         reviewService.deleteReview(review.getReviewCode());
-
+        // Assert
         verify(reviewRepo, times(1)).delete(review);
     }
 
     @Test
     void deleteReview_returnNotFoundException_whenReviewCodeDoesNotExist() {
+        // Arrange
         when(reviewRepo.findById(anyLong())).thenReturn(Optional.empty());
-
+        // Act ~ Assert
         assertThrows(NotFoundException.class, () -> reviewService.deleteReview(review.getReviewCode()));
     }
 }
